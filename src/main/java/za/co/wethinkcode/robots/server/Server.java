f164c32b5f7561b2fef5d8f4344f07213a967a7c
Herman Lintvelt <herman.lintvelt@gmail.com>
2025-04-07 10:58:14 +0200

    basic skeleton for starting

---
+++ src/main/java/za/co/wethinkcode/robots/server/Server.java
@@ -0,0 +1,17 @@
+package za.co.wethinkcode.robots.server;
+
+import za.co.wethinkcode.flow.Recorder;
+
+public class Server {
+
+    public static void main(String[] args){
+        throw new UnsupportedOperationException( "TODO" );
+    }
+
+    // The following initialisation is REQUIRED for `flow` monitoring.
+    // DO NOT REMOVE OR MODIFY THIS CODE.
+    static {
+        new Recorder().logRun();
+    }
+
+}
