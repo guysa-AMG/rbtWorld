fa9c52e3d65326c37ab24db93ca67fc744289329
landu <landujhb025@student.wethinkcode.co.za>
2026-04-14 13:11:22 +0200

    Robot State

--- src/main/java/za/co/wethinkcode/robots/server/robot/Robot.java
+++ src/main/java/za/co/wethinkcode/robots/server/robot/Robot.java
@@ -1,1 +1,20 @@
 // # State: position, direction, shields
+package za.co.wethinkcode.robots.server.robot;
+
+public class Robot {
+    private int x;
+    private int y;
+    Direction direction;
+    private int shields;
+
+    public enum Direction {
+        NORTH, SOUTH, EAST, WEST
+    }
+
+    public Robot(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.direction = Direction.NORTH;
+        this.shields = 20;
+    }
+}
