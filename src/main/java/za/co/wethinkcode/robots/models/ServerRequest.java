a755a083d70c53078cf4837b6b24ee48d2709f17
Keneilwe <kemamjhb025@student.wethinkcode.co.za>
2026-04-22 16:03:54 +0200

    fix: updated names

--- src/main/java/za/co/wethinkcode/robots/models/ServerRequest.java
+++ src/main/java/za/co/wethinkcode/robots/models/ServerRequest.java
@@ -2,27 +2,39 @@ package za.co.wethinkcode.robots.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import za.co.wethinkcode.robots.server.commands.Command;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
+
 public class ServerRequest {
 
+    public ServerRequest(String robot, String command, String[] args) {
+        this.robot = robot;
+        this.command = command;
+        this.arguments = args;
+    }
+
     @JsonProperty(required = true)
-private String robot;
+    private String robot;
 
-@JsonProperty(required = true)
-private String command;
+    @JsonProperty(required = true)
+    private String command;
 
-@JsonProperty(required = true)
-private String[] arguments;
+    @JsonProperty(required = true)
+    private String[] arguments;
 
+
+    public Command getRobot() {
+        return null;
+    }
+
+    public String getCommand() {
+        return null;
+    }
+
+    public Command getArguments() {
+        return null;
+    }
 
 
 }
