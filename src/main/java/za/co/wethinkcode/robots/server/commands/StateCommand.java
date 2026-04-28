8d2e4807b568421cbdfffc263a7d7f9bd4a24fae
Keneilwe <kemamjhb025@student.wethinkcode.co.za>
2026-04-28 16:08:33 +0200

    feature: Added StateCommand

--- src/main/java/za/co/wethinkcode/robots/server/commands/StateCommand.java
+++ src/main/java/za/co/wethinkcode/robots/server/commands/StateCommand.java
@@ -1,4 +1,22 @@
 package za.co.wethinkcode.robots.server.commands;
 
-public class BackCommand {
+import za.co.wethinkcode.robots.server.robot.Robot;
+import za.co.wethinkcode.robots.server.world.Iworld;
+
+public class StateCommand extends Command {
+
+    public StateCommand() {
+        super("state");
+    }
+
+    @Override
+    public boolean execute(Robot target, Iworld world) {
+
+        System.out.println("Robot " + target.getName() + " is at position "
+                + target.getPosition()
+                + " facing " + target.getDirection());
+
+
+        return true;
+    }
 }
