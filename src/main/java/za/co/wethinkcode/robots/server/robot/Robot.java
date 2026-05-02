7f4e90758f2fbad78f8e87c4142f90969c1e45f9
landu <landujhb025@student.wethinkcode.co.za>
2026-05-02 12:38:05 +0200

    feat: add setters, turning logic and movement methods with boundary checking

--- src/main/java/za/co/wethinkcode/robots/server/robot/Robot.java
+++ src/main/java/za/co/wethinkcode/robots/server/robot/Robot.java
@@ -81,4 +81,80 @@ public abstract class Robot extends Impediments {
 
     public void sendMessage(String string) {
     }
+
+    public void setWorldBounds(int width, int height) {
+        this.worldWidth = width;
+        this.worldHeight = height;
+    }
+
+    public void setStatus(OperationalMode status) {
+        this.status = status;
+    }
+
+    public void turnLeft() {
+        switch (this.direction) {
+            case NORTH -> this.direction = Directions.WEST;
+            case WEST  -> this.direction = Directions.SOUTH;
+            case SOUTH -> this.direction = Directions.EAST;
+            case EAST  -> this.direction = Directions.NORTH;
+        }
+    }
+
+    public void turnRight() {
+        switch (this.direction) {
+            case NORTH -> this.direction = Directions.EAST;
+            case EAST  -> this.direction = Directions.SOUTH;
+            case SOUTH -> this.direction = Directions.WEST;
+            case WEST  -> this.direction = Directions.NORTH;
+        }
+    }
+
+    public boolean moveForward(int steps) {
+        return move(steps, this.direction);
+    }
+
+    public boolean moveBack(int steps) {
+        return move(steps, getOppositeDirection(this.direction));
+    }
+
+    private boolean move(int steps, Directions direction) {
+        int x = this.position.getX();
+        int y = this.position.getY();
+
+        for (int i = 0; i < steps; i++) {
+            int newX = x;
+            int newY = y;
+
+            switch (direction) {
+                case NORTH -> newY += 1;
+                case SOUTH -> newY -= 1;
+                case EAST  -> newX += 1;
+                case WEST  -> newX -= 1;
+            }
+
+            if (isOutOfBounds(newX, newY)) {
+                break;
+            }
+
+            x = newX;
+            y = newY;
+        }
+
+        this.position = new Position(x, y);
+        return true;
+    }
+
+    private boolean isOutOfBounds(int x, int y) {
+        return x < -(worldWidth / 2) || x > (worldWidth / 2)
+                || y < -(worldHeight / 2) || y > (worldHeight / 2);
+    }
+
+    private Directions getOppositeDirection(Directions dir) {
+        return switch (dir) {
+            case NORTH -> Directions.SOUTH;
+            case SOUTH -> Directions.NORTH;
+            case EAST  -> Directions.WEST;
+            case WEST  -> Directions.EAST;
+        };
+    }
 }
