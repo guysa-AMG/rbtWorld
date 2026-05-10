3b47c7076ab698b5116ce8ddfecccf7f00f8936f
TUMO-MOGAME <tumomogame9@gmail.com>
2026-05-10 15:42:12 +0200

    Testing: Forward, Back, turn, state, Robots, look,quit, shutdown, Dump commands

---
+++ src/test/java/za/co/wethinkcode/robots/server/commands/DumpCommandTest.java
@@ -0,0 +1,46 @@
+package za.co.wethinkcode.robots.server.commands;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import za.co.wethinkcode.robots.server.robot.BaseRobot;
+import za.co.wethinkcode.robots.server.world.RobotWorld;
+
+public class DumpCommandTest {
+
+    private RobotWorld world;
+    private BaseRobot robot;
+
+    @BeforeEach
+    void setup() {
+        world = new RobotWorld(11, 11, 5);
+        world.addRobot("HAL");
+        robot = world.getAllRobots().get("HAL");
+    }
+
+    @Test
+    void execute_returnsNull() {
+        // DumpCommand currently returns null after sending a message to the robot.
+        // If this changes to return an OK response, update this test.
+        assertNull(new DumpCommand("HAL").execute(world, robot));
+    }
+
+    @Test
+    void execute_doesNotThrowWithValidRobotAndWorld() {
+        assertDoesNotThrow(() -> new DumpCommand("HAL").execute(world, robot));
+    }
+
+    @Test
+    void getCommandName_isDump() {
+        assertEquals("dump", new DumpCommand("HAL").getCommandName());
+    }
+
+    @Test
+    void getRobotName_returnsConstructorValue() {
+        assertEquals("HAL", new DumpCommand("HAL").getRobotName());
+    }
+}
